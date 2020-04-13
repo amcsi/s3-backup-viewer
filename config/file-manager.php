ee5773c9542f4d77d23937af7ff2fc284f1e1a49
Attila Szeremi <attila.szeremi@treehousetechgroup.com>
2020-04-13 18:59:20 +0200

    Include cloud in diskList

--- config/file-manager.php
+++ config/file-manager.php
@@ -25,7 +25,7 @@ return [
      * List of disk names that you want to use
      * (from config/filesystems)
      */
-    'diskList' => ['public'],
+    'diskList' => ['public', 'cloud'],
 
     /**
      * Default disk for left manager
